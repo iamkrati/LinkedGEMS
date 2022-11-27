10e15b5cb9fcbadb9c5b4b6e7cc978238e233ca9
Krati Goyal <gkrati04@gmail.com>
2022-11-27 10:16:20 +0530

    GLA_ALUMNI_PORTAL

--- about.js
+++ about.js
@@ -1,29 +1,37 @@
-    var index = 0;
-    var slides = document.querySelectorAll(".slides");
-    var dot = document.querySelectorAll(".dot");
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dot = document.querySelectorAll(".dot");
 
-    function changeSlide() {
+function changeSlide() {
 
-      if (index < 0) {
-        index = slides.length - 1;
-      }
+  if (index < 0) {
+    index = slides.length - 1;
+  }
 
-      if (index > slides.length - 1) {
-        index = 0;
-      }
+  if (index > slides.length - 1) {
+    index = 0;
+  }
 
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        dot[i].classList.remove("active");
-      }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dot[i].classList.remove("active");
+  }
 
-      slides[index].style.display = "block";
-      dot[index].classList.add("active");
+  slides[index].style.display = "block";
+  dot[index].classList.add("active");
 
-      index++;
+  index++;
 
-      setTimeout(changeSlide, 2000);
+  setTimeout(changeSlide, 2000);
 
-    }
+}
 
-    changeSlide();
+changeSlide();
+
+
+document.querySelector(".cn").addEventListener('click',function()
+{change();});
+function change() {
+  // e.preventDefault();
+  window.location.assign("menu.html");
+}
